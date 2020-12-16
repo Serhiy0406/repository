fe9cfaf27fbb6728bcec9effbe92e9c9c057d179
Serhiy0406 <pavchuk.serhiy@chnu.edu.ua>
2020-12-16 19:55:56 +0200

    last task completed

---
+++ lab_14/task_13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, string> phonebook = new SortedDictionary<string, string>();
+            while (true)
+            {
+                string[] str = Console.ReadLine().Split(' ');
+                if (str[0] == "END") { break; }
+                if (str[0] == "ListAll")
+                {
+                    foreach (var i in phonebook)
+                        Console.WriteLine($"{i.Key} --> {i.Value}");
+                    break;
+                }
+                if (str[0] == "A")
+                {
+                    phonebook[str[1]] = str[2];
+                }
+                else if(str[0]=="S")
+                {
+                    Console.WriteLine($"{str[1]} --> {phonebook[str[1]]}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
